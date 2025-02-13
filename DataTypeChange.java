26c21996e8663bb4135b0ec29f19c04ae4aae374
Mohsinshareef23 <148751368+Mohsinshareef23@users.noreply.github.com>
2025-02-13 16:03:32 +0530

    Add files via upload

---
+++ DataTypeChange.java
@@ -0,0 +1,13 @@
+package Oops;
+
+public class DataTypeChange {
+public static void main(String[] args) {
+	short s = 129;
+	byte b = (byte)s ;
+	System.out.println(s);
+	int i = 2000000000;
+	byte c = (byte)i;
+	System.out.println(i);
+	
+}
+}
